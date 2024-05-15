7a38fa294598094964cbf3848445ecb788cc1fd3
SheepYhangCN <sheepyuhang@163.com>
2024-05-15 17:28:14 +0800

    fix

--- RandomExtension.cs
+++ RandomExtension.cs
@@ -12,11 +12,11 @@ namespace SheepYhangCN.RandomExtension
 		}
 		public static bool FiftyFifty(this Random random)
 		{
-			return Choose(random,false,true);
+			return (random.Next(0,2) == 0);
 		}
 		public static T FiftyFifty<T>(this Random random,T arg1,T arg2)
 		{
-			return Choose(random,arg1,arg2);
+			return (random.Next(0,2) == 0 ? arg1 : arg2);
 		}
 		public static bool Chance(this Random random,double chance)
 		{
@@ -28,13 +28,18 @@ namespace SheepYhangCN.RandomExtension
 		}
 		public static T ChancePerItem<T>(this Random random,Dictionary<T,int> items)
 		{
-			var r = random.Next(1,items.Count);
-			var n = 0;
+            var i = 0;
+			foreach (var ii in items)
+			{
+				i += ii.Value;
+			}
+			var r = random.Next(1, i + 1);
+			var current = 0;
 			var result = default(T);
 			foreach (var a in items)
 			{
-				n += a.Value;
-				if (n >= r)
+				current += a.Value;
+				if (current >= r)
 				{
 					result = a.Key;
 					break;
